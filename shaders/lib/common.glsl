24c2b41a535bc30d3e95ecdd63f274a812fdfd2e
gri573 <david.dg.grimm@gmail.com>
2021-07-20 21:32:36 +0200

    lower default shadow resolution this is to stop people from complaining about performance

--- shaders/lib/common.glsl
+++ shaders/lib/common.glsl
@@ -195,7 +195,7 @@ Complementary Shaders by EminGT, based on BSL Shaders by Capt Tatsu
   #define CG_TM 0.0 //[0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0]
 
   const float sunPathRotation = -40.0; //[-60.0 -55.0 -50.0 -45.0 -40.0 -35.0 -30.0 -25.0 -20.0 -15.0 -10.0 -5.0 0.0 5.0 10.0 15.0 20.0 25.0 30.0 35.0 40.0 45.0 50.0 55.0 60.0]
-  const int shadowMapResolution = 2048; //[256 1024 2048 3072 4096 8192]
+  const int shadowMapResolution = 1024; //[256 512 1024 2048 3072 4096 8192]
   const float shadowDistance = 256.0; //[64.0 96.0 128.0 192.0 256.0 384.0 512.0 768.0 1024.0]
   //#define SHADOWS
   #define SHADOW_FILTER
